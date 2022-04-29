d960151c7780b32ef98b2f3e5f5f916ff421b749
hrbrmstr <bob@rud.is>
2022-04-29 05:27:27 -0400

    cleaned up udp code a bit and added a callback version

--- src/code.cpp
+++ src/code.cpp
@@ -5,8 +5,18 @@ using namespace Rcpp;
 #include <iostream>
 #include <fstream>
 #include <netinet/in.h>
+#include <unistd.h>
 #include <functional>
 #include <string.h>
+
+/* Check for interrupt without long jumping */
+void check_interrupt_fn(void *dummy) {
+  R_CheckUserInterrupt();
+}
+
+int pending_interrupt() {
+  return !(R_ToplevelExec(check_interrupt_fn, NULL));
+}
 
 //' Log local UDP Tempest messages to a file
 //'
@@ -32,8 +42,16 @@ void udp_logger(std::string path) {
 
   int port = 50222;
   int broadcast = 1;
+  int reuse_port = 1;
+  int reuse_address = 1;
+
+  bool processing = true;
 
   setsockopt(s, SOL_SOCKET, SO_BROADCAST, &broadcast, sizeof broadcast);
+  setsockopt(s, SOL_SOCKET, SO_REUSEADDR, &reuse_address, sizeof(reuse_address));
+#ifdef __APPLE__
+  setsockopt(s, SOL_SOCKET, SO_REUSEPORT, &reuse_port, sizeof(reuse_port));
+#endif
 
   memset(&si_me, 0, sizeof(si_me));
 
@@ -43,7 +61,7 @@ void udp_logger(std::string path) {
 
   ::bind(s, (sockaddr *)&si_me, sizeof(sockaddr));
 
-  while(true) {
+  while(processing) {
 
     char buf[10*1024];
     unsigned slen = sizeof(sockaddr);
@@ -56,10 +74,76 @@ void udp_logger(std::string path) {
 
     file_stream << record << std::endl;
 
-    Rcpp::checkUserInterrupt();
+    if (pending_interrupt()) {
+      processing = false;
+    }
 
   }
 
   file_stream.close();
 
+   close(s);
+
+}
+
+//' Log local UDP Tempest messages to a file
+//'
+//' This dedicates the R session to logging UDP packets to a file. No other operations
+//' can be performed until terminated.
+//'
+//' @param f R function callback (one param: length 1 chr)
+//' @references <https://weatherflow.github.io/Tempest/api/udp/v171/>
+//' @return nothing!
+//' @export
+//' @examples
+//' # udp_callback_logger(function(x) writeLines(x))
+// [[Rcpp::export]]
+void udp_callback_logger(Function f) {
+
+  struct sockaddr_in si_me, si_other;
+
+  int s = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP);
+
+  int port = 50222;
+  int broadcast = 1;
+  int reuse_port = 1;
+  int reuse_address = 1;
+
+  bool processing = true;
+
+  setsockopt(s, SOL_SOCKET, SO_BROADCAST, &broadcast, sizeof broadcast);
+  setsockopt(s, SOL_SOCKET, SO_REUSEADDR, &reuse_address, sizeof(reuse_address));
+#ifdef __APPLE__
+  setsockopt(s, SOL_SOCKET, SO_REUSEPORT, &reuse_port, sizeof(reuse_port));
+#endif
+
+  memset(&si_me, 0, sizeof(si_me));
+
+  si_me.sin_family = AF_INET;
+  si_me.sin_port = htons(port);
+  si_me.sin_addr.s_addr = INADDR_ANY;
+
+  ::bind(s, (sockaddr *)&si_me, sizeof(sockaddr));
+
+  while(processing) {
+
+    char buf[10*1024];
+    unsigned slen = sizeof(sockaddr);
+
+    recvfrom(s, buf, sizeof(buf)-1, 0, (sockaddr *)&si_other, &slen);
+
+    std::string record = buf;
+
+    record.erase(remove(record.begin(), record.end(), 0x01), record.end());
+
+    LogicalVector res = f(CharacterVector(record));
+
+    if ((is_true(all(res))) || pending_interrupt()) {
+      processing = false;
+    }
+
+  }
+
+  close(s);
+   
 }
